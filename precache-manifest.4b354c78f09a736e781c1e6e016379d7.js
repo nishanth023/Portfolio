dd300fa7b5283c1af77e96f3102b65cf6df873c7
nishanth <nishanth.ec19@bitsathy.ac.in>
2021-01-31 19:04:30 +0530

    Updates

---
+++ precache-manifest.4b354c78f09a736e781c1e6e016379d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "591eb5beb99e69ed8d3e",
+    "url": "/Portfolio/static/js/runtime~main.591eb5be.js"
+  },
+  {
+    "revision": "d4991a2d1d7ce8f49175",
+    "url": "/Portfolio/static/js/main.d4991a2d.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/Portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "d4991a2d1d7ce8f49175",
+    "url": "/Portfolio/static/css/main.1cbe92b0.chunk.css"
+  },
+  {
+    "revision": "e88fc68f719644edbcae5dbb778e8190",
+    "url": "/Portfolio/index.html"
+  }
+];
